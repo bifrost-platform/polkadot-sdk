4e372fd102ff51e80ad9799950a6cc02d3aeae9e
dnjscksdn98 <dnjscksdn98@gmail.com>
2023-10-19 15:48:40 +0900

    init bifrost-polkadot-base

--- substrate/frame/im-online/src/weights.rs
+++ substrate/frame/im-online/src/weights.rs
@@ -73,6 +73,7 @@ use core::marker::PhantomData;
 /// Weight functions needed for `pallet_im_online`.
 pub trait WeightInfo {
 	fn validate_unsigned_and_then_heartbeat(k: u32, ) -> Weight;
+	fn set_slash_fraction() -> Weight;
 }
 
 /// Weights for `pallet_im_online` using the Substrate node and recommended hardware.
@@ -99,6 +100,11 @@ impl<T: frame_system::Config> WeightInfo for SubstrateWeight<T> {
 			.saturating_add(T::DbWeight::get().writes(1_u64))
 			.saturating_add(Weight::from_parts(0, 32).saturating_mul(k.into()))
 	}
+	fn set_slash_fraction() -> Weight {
+		Weight::from_parts(18_178_000, 0)
+			.saturating_add(T::DbWeight::get().reads(6 as u64))
+			.saturating_add(T::DbWeight::get().writes(4 as u64))
+	}
 }
 
 // For backwards compatibility and tests.
@@ -123,5 +129,10 @@ impl WeightInfo for () {
 			.saturating_add(RocksDbWeight::get().reads(4_u64))
 			.saturating_add(RocksDbWeight::get().writes(1_u64))
 			.saturating_add(Weight::from_parts(0, 32).saturating_mul(k.into()))
+	}
+	fn set_slash_fraction() -> Weight {
+		Weight::from_parts(18_178_000, 0)
+			.saturating_add(RocksDbWeight::get().reads(6 as u64))
+			.saturating_add(RocksDbWeight::get().writes(4 as u64))
 	}
 }
